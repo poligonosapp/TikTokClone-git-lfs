e53fc03d147fb52505034746ed35120255c4a216
Luis Mendes <machadoluismendes@gmail.com>
2023-01-03 12:45:43 -0300

    Increment
    import { ErrorBoundary } from 'react-error-boundary';

--- Tiktok-Clone/pages/index.tsx
+++ Tiktok-Clone/pages/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bb496af75e99dce077d539f767d44a35b4fb87cc81e0a5b11e0b88955c33f77
-size 3287
+oid sha256:97e475ac07463c31bcf3e92525c4aa9aef2b6bbb85acd0490fd377bd2267f625
+size 3304
